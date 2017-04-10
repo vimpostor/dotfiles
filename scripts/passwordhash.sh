6bc17616b66a017b2741c46f32da33c67aece0f4
Magnus Groß <magnus.gross@rwth-aachen.de>
2017-04-10 13:05:47 +0200

    Remove trailing space

--- scripts/passwordhash.sh
+++ scripts/passwordhash.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -n $1 | iconv -t utf16le | openssl md4 | sed 's/(stdin)= //' | tee >(xclip -selection c) 
+echo -n $1 | iconv -t utf16le | openssl md4 | sed 's/(stdin)= //' | tee >(xclip -selection c)
