0e96d7513662fd2c97aeeb18e90c4802b1446019
Magnus Groß <magnus.gross@rwth-aachen.de>
2018-01-16 15:32:34 +0100

    Cava: Add y offset

--- scripts/transparent-cava.sh
+++ scripts/transparent-cava.sh
@@ -3,10 +3,11 @@
 # This option configures how high the window will be. 1 means fullscreen and 0 means no height.
 REL_HEIGHT='0.2'
 
+YOFFSET=10
 MONITOR_HEIGHT=$(xwininfo -root| grep -Eo 'Height.*'| cut -d ' ' -f2)
 WIDTH=$(xwininfo -root| grep -Eo 'Width.*'| cut -d ' ' -f2)
 ABS_HEIGHT=$(echo "$REL_HEIGHT * $MONITOR_HEIGHT / 1" | bc)
-YPOS=$((MONITOR_HEIGHT - ABS_HEIGHT))
+YPOS=$((MONITOR_HEIGHT - ABS_HEIGHT + YOFFSET))
 
 # if there already is an instance running, kill it
 if xdotool search --classname "transparentCava"; then
