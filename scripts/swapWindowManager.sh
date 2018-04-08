10733d7fb3b3ca883042b284baca3e3a8f3908dd
Magnus Groß <magnus.gross@rwth-aachen.de>
2018-04-09 01:18:32 +0200

    Improve swapWindowManager.sh

--- scripts/swapWindowManager.sh
+++ scripts/swapWindowManager.sh
@@ -3,5 +3,5 @@
 if pidof bspwm ; then
 	pkill compton && pkill sxhkd && pkill bspwm && kwin_x11 --replace &>/dev/null &disown
 else
-	pkill kwin_x11 && bspwm &>/dev/null &disown
+	pkill kwin_x11 && sleep 1 && bspwm &disown && sleep 3 && bspc wm -o && xdotool search --classname krunner windowclose
 fi
