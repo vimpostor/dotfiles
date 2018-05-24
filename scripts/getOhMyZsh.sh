3b31ed3639686810a1dcec6f4e1cdbc048d6c936
Magnus Groß <magnus.gross@rwth-aachen.de>
2018-05-24 18:50:20 +0200

    Add zsh and vimplug download

---
+++ scripts/getOhMyZsh.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
---
+++ scripts/getVimPlug.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
