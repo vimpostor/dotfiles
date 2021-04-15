147c7e8f21e55101874af8fad45942d174c2970e
Magnus Groß <magnus.gross@rwth-aachen.de>
2021-04-15 14:00:52 +0200

    Add poll.sh

---
+++ scripts/poll.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+FILEPATH="/tmp/.poll"
+DIFFPATH="/tmp/.poll.diff"
+GREPPATH="/tmp/.poll.grep"
+NEWFILEPATH="$FILEPATH.new"
+INTERVAL=60
+USERAGENT='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'
+CALLBACK='notify-send'
+GREP=0
+GREPSTRING=''
+CHECK=0
+ARGS="A:c:g:hn:"
+HELPMSG="This script can poll websites for changes and notifies you.
+
+Usage:
+poll.sh [OPTIONS] URL
+
+Options:
+-A USERAGENT: Useragent to use (Default: $USERAGENT).
+-c CALLBACK: Notification method, can be one of {notify-send, echo} (Default: $CALLBACK).
+-g GREP: Grep the website for a string before diffing.
+-h: Show this help message.
+-n SECONDS: Poll rate in seconds (Default: $INTERVAL).
+
+Example:
+poll.sh -n 5 'https://example.com'
+"
+
+# CLI args
+while getopts $ARGS OPT; do
+	case $OPT in
+	A)
+		USERAGENT="$OPTARG"
+		;;
+	c)
+		CALLBACK="$OPTARG"
+		;;
+	g)
+		GREP=1
+		GREPSTRING="$OPTARG"
+		;;
+	h)
+		printf "%s" "$HELPMSG"
+		exit 0
+		;;
+	n)
+		INTERVAL="$OPTARG"
+		;;
+	:)
+		echo "Error: -$OPT requires an argument"
+		exit 1
+		;;
+	?)
+		echo "Error: Unknown option -$OPT"
+		exit 1
+		;;
+	esac
+done
+shift $((OPTIND -1))
+
+URL="$*"
+
+
+# poll
+while true; do
+	curl -s -A "$USERAGENT" "$URL" -o "$NEWFILEPATH"
+	if [[ "$GREP" -eq 1 ]]; then
+		grep -Ei "$GREPSTRING" "$NEWFILEPATH" > "$GREPPATH"
+		mv "$GREPPATH" "$NEWFILEPATH"
+	fi
+	if [[ "$CHECK" -eq 1 ]]; then
+		if ! diff -q "$FILEPATH" "$NEWFILEPATH" &> /dev/null; then
+			diff "$FILEPATH" "$NEWFILEPATH" > "$DIFFPATH"
+			case "$CALLBACK" in
+			notify-send)
+				notify-send -i webbrowser -h string:x-kde-urls:"file://$DIFFPATH" 'Website Activity' "Click <a href=\"$URL\">here</a> to visit the page now!"
+				;;
+			echo)
+				echo "Activity on website $URL"
+				;;
+			*)
+				echo "Unknown notification method"
+				;;
+			esac
+		fi
+	fi
+	CHECK=1
+	mv "$NEWFILEPATH" "$FILEPATH"
+	sleep "$INTERVAL"
+done
