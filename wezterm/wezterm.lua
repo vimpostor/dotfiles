2d5cd2d3aeea955329085688ff3d7505c51e38de
Magnus Groß <magnus.gross@rwth-aachen.de>
2021-05-28 19:26:04 +0200

    Add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm';
+return {
+	check_for_updates = false,
+	color_scheme = "Breeze",
+	enable_tab_bar = false,
+	exit_behavior = "Close",
+	font = wezterm.font("MesloLGS NF"),
+	font_size = 10.0,
+}
