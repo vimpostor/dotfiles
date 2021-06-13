e8897a411fb3280fff81986a52614191f6a44685
Magnus Groß <magnus.gross@rwth-aachen.de>
2021-06-13 22:15:45 +0200

    Add mbsync notifications

---
+++ scripts/mail-notify.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# Usage: MAILDIR_INBOX=/path/to/inbox mail-notify.sh
+
+set -e
+
+# do not notify about individual messages, but summarize as soon as this number of new messages is reached
+SUMMARY_THRESHOLD=5
+MSG_CACHE="$HOME/.cache/mbsync-notify.cache"
+
+if [[ -z "$MAILDIR_INBOX" ]]; then
+	# use default maildir
+	MAILDIR_INBOX="$(find "$HOME/.local/share/mail/" -type d -name 'INBOX' | head -1)"
+fi
+MAILDIR_NEW="$MAILDIR_INBOX/new"
+if ! [[ -d "$MAILDIR_NEW" ]]; then
+	echo "Error: $MAILDIR_NEW is not a directory!"
+	exit 1
+fi
+# create cache if it doesn't exist
+if ! [[ -f "$MSG_CACHE" ]]; then
+	touch "$MSG_CACHE"
+fi
+
+# Decodes MIME RFC 2047 to UTF8
+function decode() {
+	DECODER="print decode("\""MIME-Header"\"", "\""$*"\"")"
+	DECODED="$(echo "" | perl -CS -MEncode -ne "$DECODER")"
+}
+
+# sends a notification with title and body
+function notify() {
+	notify-send -i mail-message -c email.arrived -h string:desktop-entry:org.kde.konsole "$1" "$2"
+}
+
+COUNT=$(ls -b1 "$MAILDIR_NEW" | wc -l)
+if [[ "$COUNT" -eq 0 ]]; then
+	# no new messages
+	echo 'No new messages.'
+elif [[ "$COUNT" -lt "$SUMMARY_THRESHOLD" ]]; then
+	# send a notification for each message
+	for MSG in "$MAILDIR_NEW"/*; do
+		# only notify if we haven't already for this message
+		if ! grep -Fq "$(basename "$MSG")" "$MSG_CACHE"; then
+			SENDER="$(grep -E '^From: ' $MSG | sed 's/From: //')"
+			decode "$SENDER"
+			PARSED_SENDER="$(echo "$DECODED"| sed 's/ <.*>//g')"
+			SUBJECT="$(grep -E '^Subject: ' $MSG | sed 's/Subject: //')"
+			decode "$SUBJECT"
+			notify "$PARSED_SENDER" "$DECODED"
+		fi
+	done
+else
+	# print a summary (digest) message
+	notify "You have new mail!" "$COUNT new messages."
+fi
+
+# create cache
+ls -b1 "$MAILDIR_NEW" > "$MSG_CACHE"
