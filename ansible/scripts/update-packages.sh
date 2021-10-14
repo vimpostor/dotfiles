843b68a7bf5c139a38fd0524bb098380d4939013
Magnus Groß <magnus.gross@rwth-aachen.de>
2021-10-14 12:13:07 +0200

    Explicitly include base-devel

--- ansible/scripts/update-packages.sh
+++ ansible/scripts/update-packages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-diff --color=auto -y --suppress-common-lines -- <(cat $(find . -type f -name packages.txt) | sort) <(pacman -Qqe)
+diff --color=auto -y --suppress-common-lines -- <(cat $(find . -type f -name packages.txt) <(pacman -Qgq base-devel) | sort -u) <(cat <(pacman -Qqe) <(echo base-devel) | sort -u)
