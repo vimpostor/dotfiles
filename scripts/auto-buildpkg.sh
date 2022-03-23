0da9a495eb997fe132a6e6760e2b1ff4693d7203
Magnus Groß <magnus.gross@rwth-aachen.de>
2022-03-23 01:36:16 +0100

    Delete build job notifications from history

--- scripts/auto-buildpkg.sh
+++ scripts/auto-buildpkg.sh
@@ -9,6 +9,8 @@ function decode() {
 	DECODED="$(echo "" | perl -CS -MEncode -ne "$DECODER" || echo 'Parse Error')"
 }
 
+# Example subject line:
+# Subject: AUR Out-of-date Notification for veloren-nightly-bin
 SUBJECT="$(grep -A1 -m1 '^Subject:' | grep -E '^Subject: |^\s.' | sed 's/^Subject://' |  sed 's/^\s*\|\s*$//g' | tr '\n' ' ' | sed 's/ $//')"
 decode "$SUBJECT"
 PKGNAME="${DECODED##*Notification for }"
@@ -17,5 +19,5 @@ if [ -z "$PKGNAME" ]; then
 fi
 
 # spawn background tmux window
-WINDOW_ID="$(tmux new-window -d -c ~/Documents/Miscellaneous/AUR/"$PKGNAME" -P -F '#{window_index}' "$HOME/Documents/scripts/update-pkgbuild.sh; gdbus call --session --dest=org.freedesktop.Notifications --object-path=/org/freedesktop/Notifications --method=org.freedesktop.Notifications.Notify 'makepkg' 0 'Finished' 'Package build finished' 'Job for $PKGNAME finished.' '[]' '{\"desktop-entry\": <\"org.kde.konsole\">}' 5000 >/dev/null; exec zsh")"
+WINDOW_ID="$(tmux new-window -d -c ~/Documents/Miscellaneous/AUR/"$PKGNAME" -P -F '#{window_index}' "$HOME/Documents/scripts/update-pkgbuild.sh; ID=\$(gdbus call --session --dest=org.freedesktop.Notifications --object-path=/org/freedesktop/Notifications --method=org.freedesktop.Notifications.Notify 'makepkg' 0 'Finished' 'Package build finished' 'Job for $PKGNAME finished.' '[]' '{\"desktop-entry\": <\"org.kde.konsole\">}' 5000 | grep -Eo 'uint32\s[[:digit:]]+'| cut -d' ' -f2); zsh; gdbus call --session --dest=org.freedesktop.Notifications --object-path=/org/freedesktop/Notifications --method=org.freedesktop.Notifications.CloseNotification \$ID")"
 tmux rename-window -t "$WINDOW_ID" "$PKGNAME"
