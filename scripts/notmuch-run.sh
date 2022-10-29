vimpostor/dotfiles

commit aa0ef4ead6d4dd6168e05d6e2390b34793b3dd0c

Make notmuch more quiet
Before this patch it spammed stuff on stderr like:

Note: Ignoring non-mail file: ~/.local/share/mail/*/.uidvalidty
Note: Ignoring non-mail file: ~/.local/share/mail/*/.mbsyncstate

scripts/notmuch-run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
 
-notmuch new --quiet
-
-
+notmuch new --quiet 2>&1 | grep -v 'Ignoring non-mail file'

commit 43fc7b6eecaf0f49cba9a1b4032315d98ce87aa8

notmuch: Avoid non-zero exit code when grepping
Regression was introduced in aa0ef4ead6d4dd6168e05d6e2390b34793b3dd0c

We don't want the service to fail, just because grep didn't output a
line.

scripts/notmuch-run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-notmuch new --quiet 2>&1 | grep -v 'Ignoring non-mail file'
+set -e
+
+OUTPUT="$(notmuch new --quiet 2>&1)"
+printf "$(printf "$OUTPUT" | grep -v 'Ignoring non-mail file')"