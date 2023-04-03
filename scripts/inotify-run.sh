bc5d50c7af4d1e14e1efa56eee98550728467a91
Magnus Groß <magnus@mggross.com>
2023-04-03 20:46:54 +0200

    Add inotify run script

---
+++ scripts/inotify-run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Runs the given command everytime a file changes in the current directory
+
+while inotifywait -qq -e modify .; do
+	# sleep for a short while to bundle all events in one roundtrip
+	sleep 0.2
+
+	$*
+	echo "Exited with code $?"
+done
