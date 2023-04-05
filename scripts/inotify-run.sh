253e8718b872f5a6049b86c49c8c841c508ed834
Magnus Groß <magnus@mggross.com>
2023-04-05 12:21:11 +0200

    inotify: Improve event bundling

--- scripts/inotify-run.sh
+++ scripts/inotify-run.sh
@@ -2,9 +2,13 @@
 # Runs the given command everytime a file changes in the current directory
 
 while inotifywait -qq -r -e modify .; do
-	# sleep for a short while to bundle all events in one roundtrip
+	# sleep for a short while so that all files are saved
 	sleep 0.2
-
 	"$@"
 	echo "Exited with code $?"
+
+	# Sleep for a short while to bundle all events in one roundtrip.
+	# We sleep in two separate steps in order to improve latency,
+	# as we don't want to burden the initial call with such a huge delay.
+	sleep 0.5
 done
