78b44e220ceb9a240d485c5ae0c7060a04dab62e
Magnus Groß <magnus@mggross.com>
2023-04-05 11:16:26 +0200

    inotify: Watch recursively
    Also fix whitespace problems in argument expansion.

--- scripts/inotify-run.sh
+++ scripts/inotify-run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Runs the given command everytime a file changes in the current directory
 
-while inotifywait -qq -e modify .; do
+while inotifywait -qq -r -e modify .; do
 	# sleep for a short while to bundle all events in one roundtrip
 	sleep 0.2
 
-	$*
+	"$@"
 	echo "Exited with code $?"
 done
