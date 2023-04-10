7c5b19d6be02c4df9f69abfb5bb02543ccff5ee7
Magnus Groß <magnus@mggross.com>
2023-04-10 14:04:46 +0200

    Add systray icon to mail sync
    For now this depends on yad, but in the future we can probably implement
    this in raw shell.
    
    For now this doesn't show any tooltip, in --listen mode we should be
    able to provide the tooltip text with "tooltip:STRING", but it doesn't
    seem to work, we will have to investigate this further.
    
    Nonetheless we still start in --listen mode, because this causes the
    default left-click action to no longer kill the process.
    
    Right now we should not allow the systray to be killed, because from the
    script we later call "kill" on the PID of that process, which would be
    susceptible to a TOCTOU (pid reuse for another process), if we allowed
    it to be killed.
    
    It can still be killed with a middle-click, but for now let's assume
    that the user would never do this .

--- scripts/mail-sync.sh
+++ scripts/mail-sync.sh
@@ -3,8 +3,13 @@
 # If one command fails (probably network connectivity issues), we don't need to execute the rest of them
 set -e
 
+yad --notification --image mail-inbox --listen &
+PID=$!
+
 mbsync -aq
 ~/Documents/scripts/mail-notify.sh
 ~/Documents/scripts/aliases-gen.sh
 ~/Documents/scripts/mail-queue-run.sh
 ~/Documents/scripts/notmuch-run.sh
+
+kill "$PID"
