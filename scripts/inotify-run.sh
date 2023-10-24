294171225e55e00614a7adff634bc3cb2cfa8365
Magnus Groß <magnus@mggross.com>
2023-10-24 22:46:16 +0200

    Allow inotify-run to operate async
    Passing -k will kill any passed program that blocks.
    I still do not 100% like the UX, i.e. it would be better if the run
    program were the foreground process and the inotifywait the background
    process instead of the other way around.

--- scripts/inotify-run.sh
+++ scripts/inotify-run.sh
@@ -1,11 +1,51 @@
 #!/usr/bin/env bash
 # Runs the given command everytime a file changes in the current directory
 
-while inotifywait -qq -r -e modify .; do
+KILL=false
+PID=""
+FIRST=true
+
+ARGS="hk"
+HELPMSG="This program runs the given command every time that a file in the current directory changes.
+
+Usage:
+-h: Show this help message.
+-k: Kill the command instead of waiting for it to exit, once the file changes
+"
+
+while getopts $ARGS OPT; do
+	case $OPT in
+	h)
+		printf "%s" "$HELPMSG"
+		exit 0
+		;;
+	k)
+		KILL=true
+		;;
+	:)
+		error "Error: -$OPT requires an argument"
+		;;
+	?)
+		error "Error: Unknown option -$OPT"
+		;;
+	esac
+done
+shift $((OPTIND -1))
+
+while "$FIRST" || inotifywait -qq -r -e modify .; do
+	FIRST=false
 	# sleep for a short while so that all files are saved
 	sleep 0.2
-	"$@"
-	echo "Exited with code $?"
+	if "$KILL"; then
+		if [ -n "$PID" ]; then
+			kill "$PID"
+		fi
+		"$@" &
+		PID=$!
+	else
+		"$@"
+		echo "Exited with code $?"
+	fi
 
 	# Sleep for a short while to bundle all events in one roundtrip.
 	# We sleep in two separate steps in order to improve latency,
