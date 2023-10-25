ec4deb5d3439d4b462cd14d79914513b7838292a
Magnus Groß <magnus@mggross.com>
2023-10-25 16:35:46 +0200

    gerrit: Prevent direct git pushes
    Most Gerrit documentations recommend to modify the origin push url to
    something invalid, e.g.:
    git remote set-url --push origin no_push
    
    I don't like this workflow, because it interferes with some scripts and
    aliases and let's be honest, it's just a hacky workaround, no_push is
    not a special set-url flag, it's just an invalid url.
    
    Instead I discovered that some people use pre-push hooks to accomplish
    the same behaviour [0], which is imho a much more elegant solution.
    
    My implementation is essentially a pure bash rewrite of the above
    solution.
    
    All refs are passed via stdin to pre-push hooks, so we just need to grep
    for the standard gerrit review refs.
    
    [0] https://www.dvratil.cz/2017/06/git-trick-481-prevent-accidentally-pushing-into-git-instead-of-gerrit/

--- scripts/gerrit-setup.sh
+++ scripts/gerrit-setup.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
+# This script setups some Gerrit hooks.
+# Must be called from the root of the repository.
 
 set -e
 
 # Install post-commit hook for "Change-Id: ",
 # that is needed by Gerrit to track different patchsets for the same review
 scp -pqO "scp://$(git remote get-url origin | sed 's#^\(ssh\|git\|https?\)://##' | sed 's#/.*##')"/hooks/commit-msg .git/hooks/commit-msg
-echo 'Successfully setup .git/hooks/commit-msg to generate "Change-Id:" headers'
+echo 'Created .git/hooks/commit-msg to generate "Change-Id:" headers'
+
+# Prevent accidental direct pushes to master
+cat > .git/hooks/pre-push <<'EOF'
+#!/usr/bin/env bash
+# Abort if the user is not pushing to a Gerrit review ref
+die () {
+	echo >&2 "$*"
+	exit 1
+}
+if [ -n "$NO_GERRIT" ]; then
+	exit 0
+fi
+# Pushed refs must contain a "refs/for/" ref, otherwise we are not pushing for review
+grep -q "refs/for/" || die "Error: You are pushing to git directly, which would skip the Gerrit review process entirely. Set the NO_GERRIT=1 environment variable, if this is intended. Please use the gpr alias instead, if you want to push for Gerrit review."
+EOF
+chmod +x .git/hooks/pre-push
+echo 'Created .git/hooks/pre-push to prevent direct pushes skipping Gerrit review.'
