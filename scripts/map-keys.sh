1551b05ffc322828e84309312925b106418f71b0
Magnus Groß <magnus@mggross.com>
2025-03-03 17:34:40 +0100

    Unify old xkbmap scripts

--- scripts/map-keys.sh
+++ scripts/map-keys.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-if command -v xcape &>/dev/null; then
-	# if xcape exists bind caps lock to both escape and ctrl
-	setxkbmap de -option caps:ctrl_modifier
-	xcape -e 'Caps_Lock=Escape'
-else
-	# else fall back to just binding caps lock to escape
-	setxkbmap de -option caps:escape
-fi
+setxkbmap us -option caps:escape -variant de_se_fi
