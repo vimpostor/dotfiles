b6634efd53b23469bcd53945ce7766b9c6ce8d0a
Magnus Groß <magnus@mggross.com>
2026-06-21 14:14:46 +0200

    Remove outdated tilde remap
    The inverted mapping has been resolved upstream now [0].
    
    [0] https://chiselapp.com/user/letoram/repository/arcan/info/30c66cbcec067711

--- arcan/durden/devmaps/keyboard/default.lua
+++ arcan/durden/devmaps/keyboard/default.lua
@@ -1,6 +1,5 @@
 local res = { name = [[default]], dctbl = {}, symmap = {}, map = { plain = {}, lshift = {}, ralt = {} } };
 res.map["ralt"][27] = "~";
-res.map["lshift"][41] = "~";
 res.map["plain"][43] = "\\";
 res.symmap[58] = "ESCAPE";
 return res;
